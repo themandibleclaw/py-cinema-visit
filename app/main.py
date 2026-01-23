6fde59e6c76deb19fcf62fe08b8bb9697bca46e9
claw <themandibleclaw@gmail.com>
2026-01-23 00:21:24 +0000

    implement requirements

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.cinema_staff import Cleaner
-from people.customer import Customer
+from app.people.customer import Customer
 
 
 def cinema_visit(customers: list, hall_number: int,
